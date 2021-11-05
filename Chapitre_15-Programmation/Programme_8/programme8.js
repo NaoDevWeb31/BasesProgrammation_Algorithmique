69595eb61ac95cc7f7a42484a39f37c279213656
Naomi Mango <naomi.mango@gmail.com>
2021-11-05 19:02:02 +0100

    [MAJ] Programmation - Programme 8 v2

--- Chapitre_15-Programmation/Programme_8/programme8.js
+++ Chapitre_15-Programmation/Programme_8/programme8.js
@@ -3,8 +3,15 @@ var resultat = 0;
 
 for (let i = 0; i <= 3; i++) {
   tab[i] = Math.floor(Math.random() * 21);
-  resultat += tab[i];
 }
-resultat /= tab.length;
-
+resultat = moyenne(tab);
 console.log("La moyenne est de : " + resultat);
+
+function moyenne(monTableau) {
+  var moyenne = 0;
+  for (let cpt = 0; cpt < monTableau.length; cpt++) {
+    moyenne += monTableau[cpt];
+  }
+  moyenne /= monTableau.length;
+  return moyenne;
+}
