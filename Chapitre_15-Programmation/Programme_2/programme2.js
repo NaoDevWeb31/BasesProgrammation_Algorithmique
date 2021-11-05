415fc4c21e7f74c107575ab1cfcbec5c069e0c68
Naomi Mango <naomi.mango@gmail.com>
2021-11-05 18:18:12 +0100

    [MAJ] Programmation - Programme 2 v3

--- Chapitre_15-Programmation/Programme_2/programme2.js
+++ Chapitre_15-Programmation/Programme_2/programme2.js
@@ -1,12 +1,7 @@
 var readline = require("readline-sync"); // Faire de la saisie dans le terminal
 
-var chiffre1;
-var chiffre2;
-var resultat;
-
-chiffre1 = readline.questionInt("Saisir votre chiffre ?");
-chiffre2 = readline.questionInt("Saisir votre chiffre ?");
-
-resultat = chiffre1 + chiffre2;
+var chiffre1 = readline.questionInt("Saisir votre chiffre ? ");
+var chiffre2 = readline.questionInt("Saisir votre chiffre ? ");
+var resultat = chiffre1 + chiffre2;
 
 console.log("Le résultat de la somme est : " + resultat);
