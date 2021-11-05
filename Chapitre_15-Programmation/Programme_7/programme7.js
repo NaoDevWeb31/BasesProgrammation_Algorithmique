d18a93da284713c0e01206773c8199d20787a51a
Naomi Mango <naomi.mango@gmail.com>
2021-11-05 18:52:49 +0100

    [MAJ] Programmation - Programme 7 v3

--- Chapitre_15-Programmation/Programme_7/programme7.js
+++ Chapitre_15-Programmation/Programme_7/programme7.js
@@ -1,8 +1,12 @@
-moyenne(2, 5, 10);
-moyenne(3, 100, 50);
+var moyenne1 = moyenne(2, 5, 10);
+var moyenne2 = (3, 100, 50);
+
+console.log("La première moyenne est de : " + moyenne1);
+console.log("La seconde moyenne est de : " + moyenne2);
 
 function moyenne(a, b, c) {
   var resultat = a + b + c;
-  resultat = resultat / 3;
-  console.log("La moyenne est de : " + resultat);
+  resultat /= 3;
+  return resultat;
+  //   console.log("La moyenne est de : " + resultat);
 }
