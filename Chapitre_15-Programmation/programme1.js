85b25bab2471cacbcf9ef046bc26ae873c5719d2
Naomi Mango <naomi.mango@gmail.com>
2021-11-05 18:02:33 +0100

    [MAJ] Programmation - Programme 1 v2

--- Chapitre_15-Programmation/programme1.js
+++ Chapitre_15-Programmation/programme1.js
@@ -2,8 +2,12 @@ var chiffre1;
 var chiffre2;
 var resultat;
 
-chiffre1 = 5;
-chiffre2 = 1;
+chiffre1 = Math.floor(Math.random() * 21); // aléatoire en 0 et 20
+chiffre2 = Math.floor(Math.random() * 21);
+
+console.log("Mon premier chiffre est : " + chiffre1);
+console.log("Mon second chiffre est : " + chiffre2);
+
 resultat = chiffre1 - chiffre2;
 
 console.log(resultat);
