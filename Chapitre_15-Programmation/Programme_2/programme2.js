bc849454ffc4dc1cb383bd9b7c4aa6cbb2f4055c
Naomi Mango <naomi.mango@gmail.com>
2021-11-05 18:14:30 +0100

    [NEW] Programmation - Programme 2 v1

---
+++ Chapitre_15-Programmation/Programme_2/programme2.js
@@ -0,0 +1,8 @@
+var readline = require("readline-sync"); // Faire de la saisie dans le terminal
+
+var chiffre1;
+var chiffre2;
+var resultat;
+
+chiffre1 = readline.questionInt("Saisir votre chiffre ?");
+console.log(chiffre1);
