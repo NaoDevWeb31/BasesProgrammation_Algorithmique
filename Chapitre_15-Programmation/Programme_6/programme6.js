428a03ffbad1d016a415c273f9ee2066647b7901
Naomi Mango <naomi.mango@gmail.com>
2021-11-05 18:43:43 +0100

    [MAJ] Programmation - Programme 6 v2

--- Chapitre_15-Programmation/Programme_6/programme6.js
+++ Chapitre_15-Programmation/Programme_6/programme6.js
@@ -1,11 +1,13 @@
 var readline = require("readline-sync");
 
-var chiffre = readline.questionInt("Saisir un chiffre entre 10 et 20 : ");
+do {
+  var chiffre = readline.questionInt("Saisir un chiffre entre 10 et 20 : ");
 
-if (chiffre < 10) {
-  console.log("Le chiffre est trop petit !");
-} else if (chiffre > 20) {
-  console.log("Le chiffre est trop grand !");
-} else {
-  console.log("Le chiffre est bien compris entre 10 et 20 !");
-}
+  if (chiffre < 10) {
+    console.log("Le chiffre est trop petit !");
+  } else if (chiffre > 20) {
+    console.log("Le chiffre est trop grand !");
+  } else {
+    console.log("Le chiffre est bien compris entre 10 et 20 !");
+  }
+} while (chiffre < 10 || chiffre > 20);
